42410765ece80036e19e6707680d69430a22a0b6
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-07-10 17:19:45 -0300

    changing the route

--- lib/date/student_date.dart
+++ lib/date/student_date.dart
@@ -5,24 +5,26 @@ import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 import 'package:xms/model/student.dart';
 
-const baseUrl = 'http://localhost:3333/student';
+const baseUrl = 'http://10.20.30.16:3333/student';
 
 class ListStudentDate {
   final http.Client httpStudant;
   ListStudentDate({@required this.httpStudant});
 
   getAll() async {
-    print(await httpStudant.get(baseUrl));
+    //print(await httpStudant.get(baseUrl));
     try {
       var response = await httpStudant.get(baseUrl);
-      print(response.statusCode);
-      if(response.statusCode == 200){
+
+      if (response.statusCode == 200) {
         Iterable jsonResponse = json.decode(response.body);
-        List<StudentModel> listStudent = jsonResponse.map((model) => StudentModel.fromJson(model)).toList();
+        List<StudentModel> listStudent =
+            jsonResponse.map((model) => StudentModel.fromJson(model)).toList();
         print(listStudent);
         return listStudent;
-      }else print ('erro');
-    } catch(_){
+      } else
+        print('erro');
+    } catch (_) {
       print('catch');
     }
   }
